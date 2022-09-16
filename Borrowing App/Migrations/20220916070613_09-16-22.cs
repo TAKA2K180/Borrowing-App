7058a6d37a1f6bd8f1282ecff27744d8137a0629
pordie01 <pordie01@gmail.com>
2022-09-16 15:07:37 +0800

    09-16-22

---
+++ Borrowing App/Migrations/20220916070613_09-16-22.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Borrowing_App.Migrations
+{
+    public partial class _091622 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                schema: "Identity",
+                table: "Borrower",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                schema: "Identity",
+                table: "Borrower");
+        }
+    }
+}
